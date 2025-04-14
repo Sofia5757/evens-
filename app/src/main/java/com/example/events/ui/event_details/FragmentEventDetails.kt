b1f2ed1556a3c000e9e3d3e729884f2048f666c8
sofia <lubinskaasona@yandex.ru>
2025-04-14 15:37:26 +0300

    !!!!!.((((.

--- app/src/main/java/com/example/events/ui/event_details/FragmentEventDetails.kt
+++ app/src/main/java/com/example/events/ui/event_details/FragmentEventDetails.kt
@@ -3,4 +3,12 @@ package com.example.events.ui.event_details
 import androidx.fragment.app.Fragment
 
 class FragmentEventDetails: Fragment() {
+
+    <string name="users_search_hint">Введите имя или класс</string>
+    <string name="info_participants">Посетители</string>
+    <string name="participate">Участвовать</string>
+    <string name="cancel_participation">Отменить запись</string>
+    <string name="data_and_time">Название</string>
+    <string name="place">Место</string>
+    <string name="mainainer">Сопровождающий</string>
 }
--- app/src/main/java/com/example/events/ui/event_participants/ViewModelParticipants.kt
+++ app/src/main/java/com/example/events/ui/event_participants/ViewModelParticipants.kt
@@ -3,4 +3,11 @@ package com.example.events.ui.event_participants
 import androidx.lifecycle.ViewModel
 
 class ViewModelParticipants: ViewModel() {
+    <string name="users_search_hint">Введите имя или класс</string>
+    <string name="info_participants">Посетители</string>
+    <string name="participate">Участвовать</string>
+    <string name="cancel_participation">Отменить запись</string>
+    <string name="data_and_time">Название</string>
+    <string name="place">Место</string>
+    <string name="mainainer">Сопровождающий</string>
 }
